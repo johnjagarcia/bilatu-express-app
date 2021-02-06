c13c349959accaa5dc3f86cdae441b5abb998ae7
John Ja García <john.garcia@release.com.co>
2021-02-06 15:23:33 -0500

    Se corrige nombre del builder de subcategorías

--- src/modules/subcategory/domain/SubcategoryBuilder.ts
+++ src/modules/subcategory/domain/SubcategoryBuilder.ts
@@ -1,7 +1,7 @@
 import { ObjectBuilder } from "ts-object-builder";
 import Subcategory from "./Subcategory";
 
-export default class ProductCategoryBuilder extends ObjectBuilder<Subcategory> {
+export default class SubategoryBuilder extends ObjectBuilder<Subcategory> {
   constructor() {
     super(Subcategory);
   }
